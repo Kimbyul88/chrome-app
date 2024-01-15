cd921d0bb7361bd9cf1397ed4a5ae25a191d739d
Kimbyul88 <byul88byul@gmail.com>
2024-01-15 22:04:58 +0900

    Login 화면까지 구성완료~!
    - 로컬 스토리지 활용!

---
+++ real/app.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+// const loginButton = loginForm.querySelector("button");
+const link = document.querySelector("a");
+const h1 = document.querySelector("#greeting");
+
+const HIDDEN_MSG = "hidden";
+const USERNAME_KEY = "username";
+
+function greeting() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  h1.classList.remove(HIDDEN_MSG);
+  h1.innerText = "Hi, " + username + "!";
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_MSG);
+  greeting();
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_MSG);
+  loginForm.addEventListener("submit", handleSubmit);
+} else {
+  greeting();
+}
